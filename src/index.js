be61b14afafcd98ed0b1088c6518832a7f533175
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-27 21:03:09 +0100

    JSX syntax part2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const name = 'Josh Perez'
-const element = <h1>Hello, {name}</h1>
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName
+}
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+}
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+)
 
 ReactDOM.render(
     element, document.getElementById('root'));
