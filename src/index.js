fa3ff695083eac85dcbabe35944583ed12619d7b
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-27 21:10:08 +0100

    Rendering element - current date

--- src/index.js
+++ src/index.js
@@ -3,20 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    )
+    ReactDOM.render(element, document.getElementById('root'))
 }
 
-const user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
-}
+setInterval(tick, 1000)
 
-const element = (
-    <h1>
-        Hello, {formatName(user)}!
-    </h1>
-)
-
-ReactDOM.render(
-    element, document.getElementById('root'));
+// ReactDOM.render(
+//     element, document.getElementById('root'));
