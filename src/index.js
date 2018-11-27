296b7a7c5d56c998b370fa92fca64aec47d6ffba
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-27 21:19:24 +0100

    Composing components

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Welcome extends React.Component {
-    render() {
-        return <h1>Hello, {this.props.name}</h1>
-    }
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>
 }
 
-const element = <Welcome name="Krystian" />
+function App() {
+    return (
+        <div>
+            <Welcome name="Krystian" />
+            <Welcome name="Sara" />
+            <Welcome name="Cahal" />
+        </div>
+    )
+}
 
 ReactDOM.render(
-    element, document.getElementById('root'));
+    <App />, document.getElementById('root'));
