89f4bf79c6bbca11e833bc712f8904dd6d60a907
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-27 21:00:29 +0100

    JSX syntax

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+const name = 'Josh Perez'
+const element = <h1>Hello, {name}</h1>
+
+ReactDOM.render(
+    element, document.getElementById('root'));
