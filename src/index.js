885a2ccfdb062de837bc59d064015082027dbfa1
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-28 19:48:24 +0100

    Clock ticking - class component

--- src/index.js
+++ src/index.js
@@ -3,19 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>
+class Clock extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.props.date.toLocaleTimeString()}</h2>
+            </div>
+        )
+    }
 }
 
-function App() {
-    return (
-        <div>
-            <Welcome name="Krystian" />
-            <Welcome name="Sara" />
-            <Welcome name="Cahal" />
-        </div>
+function tick() {
+    ReactDOM.render(
+        <Clock date={new Date()} />,
+        document.getElementById('root')
     )
 }
 
-ReactDOM.render(
-    <App />, document.getElementById('root'));
+setInterval(tick, 1000)
