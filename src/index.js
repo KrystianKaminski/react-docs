40c6b82a60604bd8dc6c42507084f96f7597e658
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-28 20:19:13 +0100

    Conditional rendering part 2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleLoginClick = this.handleLoginClick.bind(this)
+        this.handleLogoutClick = this.handleLogoutClick.bind(this)
+        this.state = { isLoggedIn: false }
+    }
+
+    handleLoginClick() {
+        this.setState({ isLoggedIn: true })
+    }
+
+    handleLogoutClick() {
+        this.setState({ isLoggedIn: false })
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn
+        let button
+
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick} />
+        }
+
+        return (
+            <div>
+                <Greeting isLoggedIn={isLoggedIn} />
+                {button}
+            </div>
+        )
+    }
+}
+
 function UserGreeting(props) {
     return <h1>Welcome back!</h1>
 }
@@ -13,14 +48,29 @@ function GuestGreeting(props) {
 
 function Greeting(props) {
     const isLoggedIn = props.isLoggedIn
-
     if (isLoggedIn) {
         return <UserGreeting />
     }
     return <GuestGreeting />
 }
 
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    )
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    )
+}
+
 ReactDOM.render(
-    <Greeting isLoggedIn={true} />,
+    <LoginControl />,
     document.getElementById('root')
 )
